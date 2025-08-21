9d67b19f3dd7999a13f361085ba5c8e435293d06
Fons van der Plas <fonsvdplas@gmail.com>
2025-08-21 17:10:02 +0200

    Use Artifacts for offline support (#1)

--- src/PlotlyDemoPackage.jl
+++ src/PlotlyDemoPackage.jl
@@ -1,16 +1,25 @@
 module PlotlyDemoPackage
 import JSON
+using Artifacts
+using Base64
 
+# Get artifact paths
+const plotly_artifact_path = artifact"plotly_js"
+const mathjax_artifact_path = artifact"mathjax_js"
+const plotly_js_file = joinpath(plotly_artifact_path, "plotly.min.js")
+const mathjax_js_file = joinpath(mathjax_artifact_path, "tex-svg-full.js")
+const plotly_local_url = "data:application/javascript;base64,$(Base64.base64encode(read(plotly_js_file)))"
+const mathjax_local_url = "data:application/javascript;base64,$(Base64.base64encode(read(mathjax_js_file)))"
+
+# CDN URLs
 const plotly_cdn_url = "https://cdn.jsdelivr.net/npm/plotly.js-dist-min@3.1.0/plotly.min.js"
 const mathjax_cdn_url = "https://cdn.jsdelivr.net/npm/mathjax@3.2.2/es5/tex-svg-full.js"
 
-
+# Switch between the two
+const use_cdn = Ref(true)
 
 
 export plot
-
-
-
 
 function generate_plotly_data_object(x, y)
     # This is implemented by your package
@@ -44,6 +53,7 @@ struct _MyPlotType
     config
 end
 
+
 function plot(x::AbstractVector{<:Real}, y::AbstractVector{<:Real})
     _MyPlotType(
         generate_plotly_data_object(x, y),
@@ -55,16 +65,28 @@ end
 function Base.show(io::IO, m::MIME"text/html", p::_MyPlotType)
     id = String(rand('a':'z',16))
 
-    # You could also use HypertextLiteral.jl for this to make it faster an nicer looking.
+    # You could also use HypertextLiteral.jl for this to make it faster and nicer looking.
 	out = """
 	<div id=$id></div>
-	<script type="module">
+    
+    <!-- The asset URLs are included as link elements, instead of interpolated directly into JS. This is a nice semantic way to do it, and it increases performance when using data URIs (because HTML is easier to parse than JS). -->
+    
+    <link id="mathjax-julia" href="$(use_cdn[] ? mathjax_cdn_url : mathjax_local_url)" >
+    <link id="plotlyjs-julia" href="$(use_cdn[] ? plotly_cdn_url : plotly_local_url)" >
+    
+	<script type="module" id="plotly-julia-script">
+    
+    // Both MathJax and Plotly can be imported with ES6, but the package itself is still exposed as a global `window` variable, not through an ES6 `export`.
+    // This means that it's best to only import the package once, since different imports would collide. (It might already be imported from a different URL on this page, so we can't rely on ES6 import deduplication). 
+    
 	if(window.MathJax == null)
-		await import("$mathjax_cdn_url")
+        // Calling `await import` will load the library, and return when loaded.
+		await import(document.querySelector(`link#mathjax-julia`).href)
 	
 	if(window.Plotly == null)
-		await import("$plotly_cdn_url")
+		await import(document.querySelector(`link#plotlyjs-julia`).href)
 
+    // At this point, both MathJax and Plotly are loaded and defined on `window`.
 	Plotly.newPlot(
 		"$id", 
 		$(JSON.json(p.data)), 
@@ -77,19 +99,7 @@ function Base.show(io::IO, m::MIME"text/html", p::_MyPlotType)
     write(io, out)
 end
 
-
-
+# This adds support for the "VS Code Julia" extension "Plot panel".
 Base.show(io::IO, m::MIME"juliavscode/html", p::_MyPlotType) = Base.show(io, MIME"text/html"(), p)
-
-
-
-
-
-
-    
-    
-    
-
-
 
 end
