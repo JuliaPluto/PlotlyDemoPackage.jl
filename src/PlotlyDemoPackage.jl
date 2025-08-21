61dc530cd4f5c604ee5633ed067e534b7cb2dd3f
Fons van der Plas <fonsvdplas@gmail.com>
2025-08-21 15:14:52 +0200

    Use Artifacts for offline support

--- src/PlotlyDemoPackage.jl
+++ src/PlotlyDemoPackage.jl
@@ -1,14 +1,24 @@
 module PlotlyDemoPackage
 import JSON
+using Artifacts
+using Base64
+
+# Get artifact paths
+const plotly_artifact_path = artifact"plotly_js"
+const mathjax_artifact_path = artifact"mathjax_js"
+const plotly_js_file = joinpath(plotly_artifact_path, "plotly.min.js")
+const mathjax_js_file = joinpath(mathjax_artifact_path, "tex-svg-full.js")
+const plotly_local_url = "data:application/javascript;base64,$(Base64.base64encode(read(plotly_js_file)))"
+const mathjax_local_url = "data:application/javascript;base64,$(Base64.base64encode(read(mathjax_js_file)))"
+
 
 const plotly_cdn_url = "https://cdn.jsdelivr.net/npm/plotly.js-dist-min@3.1.0/plotly.min.js"
 const mathjax_cdn_url = "https://cdn.jsdelivr.net/npm/mathjax@3.2.2/es5/tex-svg-full.js"
 
-
+const use_cdn = Ref(false)
 
 
 export plot
-
 
 
 
@@ -44,6 +54,9 @@ struct _MyPlotType
     config
 end
 
+
+
+
 function plot(x::AbstractVector{<:Real}, y::AbstractVector{<:Real})
     _MyPlotType(
         generate_plotly_data_object(x, y),
@@ -60,10 +73,10 @@ function Base.show(io::IO, m::MIME"text/html", p::_MyPlotType)
 	<div id=$id></div>
 	<script type="module">
 	if(window.MathJax == null)
-		await import("$mathjax_cdn_url")
+		await import("$(use_cdn[] ? mathjax_cdn_url : mathjax_local_url)")
 	
 	if(window.Plotly == null)
-		await import("$plotly_cdn_url")
+		await import("$(use_cdn[] ? plotly_cdn_url : plotly_local_url)")
 
 	Plotly.newPlot(
 		"$id", 
